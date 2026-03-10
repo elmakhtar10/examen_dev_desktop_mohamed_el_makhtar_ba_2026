6d95251caaa1ede61cd84781ff3d6b5837179085
elmakhtar10 <mohamedelmakhtarba@gmail.com>
2026-03-10 14:03:06 +0000

    Ajout de lombok pour les getters/setters

--- src/main/java/com/tarma/examen_javafx_mohamed_el_makhtar_ba_l3gl/SessionUtilisateur.java
+++ src/main/java/com/tarma/examen_javafx_mohamed_el_makhtar_ba_l3gl/SessionUtilisateur.java
@@ -1,20 +1,14 @@
 package com.tarma.examen_javafx_mohamed_el_makhtar_ba_l3gl;
 
 import com.tarma.examen_javafx_mohamed_el_makhtar_ba_l3gl.model.Utilisateur;
+import lombok.Data;
 
+@Data
 public final class SessionUtilisateur {
 
     private static Utilisateur utilisateurConnecte;
 
     private SessionUtilisateur() {
-    }
-
-    public static Utilisateur getUtilisateurConnecte() {
-        return utilisateurConnecte;
-    }
-
-    public static void setUtilisateurConnecte(Utilisateur utilisateur) {
-        utilisateurConnecte = utilisateur;
     }
 
     public static void deconnecter() {
